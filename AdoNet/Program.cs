e18a1db8f3d6b33b705e36fe2ce5a2af09df8a1a
Ug114 <ug114@bk.ru>
2018-10-23 13:34:21 +0400

    New query

--- AdoNet/Program.cs
+++ AdoNet/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Data;
 
 namespace AdoNet
 {
@@ -16,7 +17,6 @@ namespace AdoNet
                 //Console.WriteLine("Connection state = " + connection.State);
 
                 var sql = "SELECT COUNT(*) FROM Items";
-
                 using (var command = new SqlCommand(sql, connection))
                 {
                     var itemsCount = (int) command.ExecuteScalar();
@@ -24,44 +24,60 @@ namespace AdoNet
                 }
 
                 sql = "INSERT INTO Categories(Name) VALUES(N'Сухофрукты')";
-
                 using (var command = new SqlCommand(sql, connection))
                 {
                     command.ExecuteNonQuery();
                 }
 
                 sql = "INSERT INTO Items(Name, CategoryId) VALUES(N'Лимоны', 5)";
-
                 using (var command = new SqlCommand(sql, connection))
                 {
                     command.ExecuteNonQuery();
                 }
 
                 sql = "UPDATE Items SET [Name] = N'Апельсины' WHERE [Id] = 1";
-
                 using (var command = new SqlCommand(sql, connection))
                 {
                     command.ExecuteNonQuery();
                 }
 
                 sql = "DELETE FROM Items WHERE [Name] = N'Апельсины'";
-
                 using (var command = new SqlCommand(sql, connection))
                 {
                     command.ExecuteNonQuery();
                 }
 
-                sql = "SELECT Items.Id, Items.Name, Categories.Name FROM Items INNER JOIN Categories ON Items.CategoryId = Categories.Id";
-
+                sql = "SELECT Items.Id, Items.Name, Categories.Name as CategoryName FROM Items INNER JOIN Categories ON Items.CategoryId = Categories.Id";
                 using (var command = new SqlCommand(sql, connection))
                 {
                     using (var reader = command.ExecuteReader())
                     {
+                        Console.WriteLine("Using reader:");
+
                         while (reader.Read())
                         {
                             Console.WriteLine("{0} {1} {2}", reader[0], reader[1], reader[2]);
                         }
+
+                        Console.WriteLine();
                     }
+                }
+                
+                var items = new SqlDataAdapter(sql, connection);
+                var dataSet = new DataSet("Items");
+                items.FillSchema(dataSet, SchemaType.Source, "Items");
+                items.Fill(dataSet, "Items");
+
+                var tableItems = dataSet.Tables["Items"];
+
+                Console.WriteLine("Using SqlDataAdapter:");
+
+                foreach (DataRow drCurrent in tableItems.Rows)
+                {
+                    Console.WriteLine("{0} {1} {2}",
+                    drCurrent["Id"],
+                    drCurrent["Name"],
+                    drCurrent["CategoryName"]);
                 }
             }
         }
